34ca736f47176598904e35c481335f1ab8459ef8
golfape <golfape@gmail.com>
2018-04-16 10:42:07 -0400

    second commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pynearest',
+  packages = ['pynearest'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Pure python online nearest neighbours',
+  author = 'GolfApe',
+  author_email = 'golfape@gmail.com',
+  url = 'https://github.com/golfape/pynearest', # use the URL to the github repo
+  download_url = 'https://github.com/golfape/pynearest/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['nearest neighbour', 'nearest','neighbour'], # arbitrary keywords
+  classifiers = [],
+)
